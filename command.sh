4d477fd4a36b2225010477b3eb25e1acbae4d2d9
web <932549144@qq.com>
2021-05-24 21:22:31 +0800

    fix 英文环境乱码无法复制的问题

--- command.sh
+++ command.sh
@@ -4,6 +4,7 @@ curl -O http://idea.medeming.com/a/jihuoma.zip
 ditto -V -x -k --sequesterRsrc --rsrc jihuoma.zip dist
 cd dist
 pbcopy < \(通用激活码\)2018.2之后的版本用这个.txt
+pbcopy < \(Õ®”√º§ªÓ¬Î\)2018.2÷Æ∫Ûµƒ∞Ê±æ”√’‚∏ˆ.txt
 cd ..
 echo "清除残余垃圾......"
 rm jihuoma.zip
