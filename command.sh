5ced4a3c5f106f4d830c03477811a9455251870d
web <46352788+web-97@users.noreply.github.com>
2021-06-16 21:16:18 +0800

    fix

--- command.sh
+++ command.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd ~
 curl -O http://idea.medeming.com/a/jihuoma1.zip
-ditto -V -x -k --sequesterRsrc --rsrc jihuoma.zip dist
+ditto -V -x -k --sequesterRsrc --rsrc jihuoma1.zip dist
 cd dist
 pbcopy < 2018.2之后的版本用这个.txt
 pbcopy < 2018.2÷Æ∫Ûµƒ∞Ê±æ”√’‚∏ˆ.txt
 cd ..
 echo "清除残余垃圾......"
-rm jihuoma.zip
+rm jihuoma1.zip
 rm -rf dist
 echo "最新激活码已复制到粘贴板"
