1b5cd3b154192cc31cf9a467db45cc45ddfcb397
web <46352788+web-97@users.noreply.github.com>
2021-08-18 10:22:57 +0800

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd ~
 curl -O https://idea.medeming.com/a/jihuoma1.zip
-ditto -V -x -k --sequesterRsrc --rsrc IDEA专用激活码8月17号.zip dist
+ditto -V -x -k --sequesterRsrc --rsrc jihuoma1.zip dist
 cd dist
 pbcopy < 2018.2月之后用这个.txt
 pbcopy < 2018.2月之后用这个.txt
 cd ..
 echo "清除残余垃圾......"
-rm IDEA专用激活码8月17号.zip
+rm jihuoma1.zip
 rm -rf dist
 echo "最新激活码已复制到粘贴板"
