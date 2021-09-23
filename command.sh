c72a1428dc433294e578a459eeb830a25932f2d6
web <46352788+web-97@users.noreply.github.com>
2021-09-23 11:56:50 +0800

    Update command.sh

--- command.sh
+++ command.sh
@@ -2,10 +2,8 @@
 cd ~
 curl -O https://idea.medeming.com/a/jihuoma1.zip
 ditto -V -x -k --sequesterRsrc --rsrc jihuoma1.zip dist
-cd dist
-pbcopy < 84EPE02Y6A - for 2018.2 or later.txt
+open dist
 cd ..
 echo "清除残余垃圾......"
 rm jihuoma1.zip
-rm -rf dist
 echo "最新激活码已复制到粘贴板"
